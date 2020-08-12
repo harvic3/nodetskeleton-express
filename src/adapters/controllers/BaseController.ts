da689360d370823c95de565797819b99a8d56e1b
Vickodev <harvic3@gmail.com>
2020-08-11 21:49:18 -0500

    independent project created for express js

--- src/adapters/controllers/BaseController.ts
+++ src/adapters/controllers/BaseController.ts
@@ -1,23 +1,3 @@
-// For KoaJs
-// import { Router, Context } from "../../infrastructure/server/CoreModules";
-// import { IResult } from "../../application/shared/result/Result.interface";
-
-// export default class BaseController {
-//   constructor() {
-//     this.router = new Router();
-//   }
-//   router: Router;
-//   HandleResult(ctx: Context, result: IResult): void {
-//     ctx.status = result.statusCode;
-//     if (result.success) {
-//       ctx.body = result.message ? result.ToResultDto() : result.ToResultDto().data;
-//     } else {
-//       ctx.body = result.ToResultDto();
-//     }
-//   }
-// }
-
-// For ExpressJs
 import { IResult } from "../../application/shared/result/Result.interface";
 import { Router, Response, RouterType } from "../../infrastructure/server/CoreModules";
 
--- src/infrastructure/middlewares/localization/index.ts
+++ src/infrastructure/middlewares/localization/index.ts
@@ -1,16 +1,3 @@
-// For KoaJs
-// import config from "../../config";
-// import { Context, Next } from "../../server/CoreModules";
-// import resources from "../../../application/shared/locals/index";
-
-// export default function () {
-//   return async function (ctx: Context, next: Next): Promise<void> {
-//     resources.Init(ctx.headers.acceptLanguage || config.params.defaultLang);
-//     await next();
-//   };
-// }
-
-// For ExpressJS
 import config from "../../config";
 import { Request, Response, NextFunction } from "../../server/CoreModules";
 import resources from "../../../application/shared/locals/index";
